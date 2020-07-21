cd8d2a5f3c1e3073b0455c4b63d9eac3b8c87285
DEEP DEY <63514087+Deep-Dey@users.noreply.github.com>
2020-07-21 06:55:17 +0530

    Content Added

--- GDP_Scrap.py
+++ GDP_Scrap.py
@@ -3,7 +3,12 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+##########################################################################  Scrap
 url="https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)"
 
 # Make a GET request to fetch the raw HTML content
@@ -45,31 +50,47 @@ for table, heading in zip(gdp_table_data[1].find_all("table"), headings):
 
     # Put the data for the table with his heading.
     data[heading] = table_data
-
-i=int(input("Press 1 to view GDP\nPress any key to Store details as CSV..."))
+##########################################################################################   Export to CSV
+i=int(input("Press 1 to view GDP and store as CSV\nPress any key to Store only details as CSV..."))
 print()
-if i==1:
-    for topic, table in data.items():
-        print(topic)
-        headers = [ "Country/Territory", "GDP(US$million)", "Rank" ]
-        print(headers)
-        for row in table:
-            if(row):
-                print(row)
-        print()
-    
+
 for topic, table in data.items():
     directory_path = os.path.join(os.getcwd(), 'OUTPUT/')
     if not os.path.exists(directory_path):
                 os.mkdir(directory_path)
                 
     # Create csv file for each table
-    with open( directory_path + topic + '.csv', 'w',  newline='') as out_file:
+    with open(directory_path+topic + '.csv', 'w',  newline='') as out_file:
         # Each 3 table has headers as following
-        headers = [ "Country/Territory", "GDP(US$million)", "Rank" ] # == t_headers
+        headers = ["Rank", "Country/Territory", "GDP(US$million)"] # == t_headers
         writer = csv.writer(out_file)
         writer.writerow(headers)
         for row in table:
             if(row):
                 writer.writerow(row)
+#######################################################################   Use panda to show in a table
+    if i == 1:
+        print(topic)
+        df=pd.read_csv(directory_path+topic + '.csv', encoding="ISO-8859-1")
+        new_df = df.replace([''], np.NaN)
+        print(new_df)
+#######################################################################
+        choice=input("Do you want to see the details as Bar Chart......(y/n)")
+        if choice=='y':
+            n=int(input("For how many countries you want to see the data Sheet..."))
+            new_df=new_df[1:n+1]
+
+            sns.set_style("ticks")
+            plt.figure(figsize=(30, 20))
+            plt.barh(new_df['Country/Territory'], new_df['GDP(US$million)'], align='center', color='blue')
+            plt.xlabel('GDP Amount', fontsize=15)
+            plt.ylabel('Country Name', fontsize=15)
+            plt.xticks(fontsize=10)
+            plt.yticks(fontsize=10)
+            plt.title(topic, fontsize = 15 )
+
+            for index, value in enumerate(new_df["GDP(US$million)"]):
+                plt.text(value, index, str(value), fontsize=10)
+            plt.show()
+
 
