50605f3d4a98d8fb0d5a94a184985b71e218f55c
DEEP DEY <63514087+Deep-Dey@users.noreply.github.com>
2020-07-23 18:33:36 +0530

    Updated

--- GDP_Scrap.py
+++ GDP_Scrap.py
@@ -1,4 +1,4 @@
-# importing the libraries
+########################################################################### importing the libraries
 from bs4 import BeautifulSoup
 import requests
 import csv
@@ -16,39 +16,26 @@ html_content = requests.get(url).text
 
 # Parse the html content
 soup = BeautifulSoup(html_content, "lxml")
-# print(soup.prettify()) # print the parsed data of html
-
-# for link in soup.find_all("a"):
-#     print("Inner Text: {}".format(link.text))
-#     print("Title: {}".format(link.get("title")))
-#     print("href: {}".format(link.get("href")))
 
 gdp_table = soup.find("table", attrs={"class": "wikitable"})
-gdp_table_data = gdp_table.tbody.find_all("tr")  # contains 2 rows
+gdp_table_data = gdp_table.tbody.find_all("tr")
 
-# Get all the headings of Lists
 headings = []
 for td in gdp_table_data[0].find_all("td"):
     # remove any newlines and extra spaces from left and right
-    headings.append(td.b.text.replace('\n', ' ').strip()) 
-# print(headings)
+    headings.append(td.b.text.replace('\n', ' ').strip())
 
 data = {}
 for table, heading in zip(gdp_table_data[1].find_all("table"), headings):
-    # Get headers of table i.e., Rank, Country, GDP.
     
     table_data = []
-    for tr in table.tbody.find_all("tr"): # find all tr's from table's tbody
+    for tr in table.tbody.find_all("tr"):
         t_row = []
-        # Each table row is stored in the form of
-        # t_row = {'Rank': '', 'Country/Territory': '', 'GDP(US$million)': ''}
 
-        # find all td's(3) in tr and zip it with t_header
         for td in tr.find_all("td"): 
             t_row.append(td.text.replace('\n', '').strip())
         table_data.append(t_row)
 
-    # Put the data for the table with his heading.
     data[heading] = table_data
 ##########################################################################################   Export to CSV
 i=int(input("Press 1 to view GDP and store as CSV\nPress any key to Store only details as CSV..."))
@@ -58,23 +45,22 @@ for topic, table in data.items():
     directory_path = os.path.join(os.getcwd(), 'OUTPUT/')
     if not os.path.exists(directory_path):
                 os.mkdir(directory_path)
-                
-    # Create csv file for each table
+
     with open(directory_path+topic + '.csv', 'w',  newline='') as out_file:
-        # Each 3 table has headers as following
-        headers = ["Rank", "Country/Territory", "GDP(US$million)"] # == t_headers
+
+        headers = ["Rank", "Country/Territory", "GDP(US$million)"]
         writer = csv.writer(out_file)
         writer.writerow(headers)
         for row in table:
             if(row):
                 writer.writerow(row)
-#######################################################################   Use panda to show in a table
+###############################################################################################   Use panda to show in a table
     if i == 1:
         print(topic)
         df=pd.read_csv(directory_path+topic + '.csv', encoding="ISO-8859-1")
         new_df = df.replace([''], np.NaN)
         print(new_df)
-#######################################################################
+################################################################################################
         choice=input("Do you want to see the details as Bar Chart......(y/n)")
         if choice=='y':
             n=int(input("For how many countries you want to see the data Sheet..."))
